c55647920a444a77ae8ed32706c4b0bec45a780a
Sanjeev Midda <s.midda1@gmail.com>
2026-01-31 08:54:54 +0000

    set up basic event listener and basic outline of function to change color

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./index.css";
 
 function App() {
-  return <div className="App"></div>;
+  const changeColor = () => {};
+
+  return <div className="App" onClick={changeColor}></div>;
 }
 
 export default App;
+
+// When I click on the main page screen, the color changes to a new one
+// - INPUT: click on screen OUTPUT: screen color changes
+
+// Select main element
+// Create event listener
+// Create function to change color of it's style
